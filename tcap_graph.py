b43a39ab0ad021d2764e9695ebdd73989d2402c2
coolguycam <hawtrods@gmail.com>
2021-02-15 22:03:30 -0500

    increased graph query to 225

--- tcap_graph.py
+++ tcap_graph.py
@@ -2,14 +2,15 @@ import requests
 import json
 
 url = "https://api.thegraph.com/subgraphs/name/cryptexglobal/tcap-rinkeby"
-json = {'query': '{vaults(where: {currentRatio_gt:"0"}) {vaultId owner debt collateral currentRatio address}}'}
+json = {'query': '{vaults(where: {currentRatio_gt:"0", currentRatio_lt:"225"}) {vaultId owner debt collateral currentRatio address}}'}
 response = requests.post(url=url, json=json)
 _data = response.json()
 vaults = []
 if _data['data'] == []:
-    print('No vaults with currentRatio under 205')
+    print('No vaults with currentRatio under 225')
 else:
     for n  in _data['data']['vaults']:
         vaults.append(int(n.get('vaultId')))
 
 print(_data)
+print(vaults)
